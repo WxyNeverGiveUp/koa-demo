bfe176a496a0b052ed8823943b5e5cf1eb21cf5e
wuxy720 <17678398501@163.com>
2019-04-16 11:11:44 +0800

    file update

--- src/routes/router.ts
+++ src/routes/router.ts
@@ -1,7 +1,7 @@
 import * as Router from 'koa-router'
 import * as fs from 'fs'
 import * as path from 'path'
-import { api } from '../api' // api对象
+import * as Koa from 'koa'
 
 const router = new Router();
 
@@ -9,14 +9,14 @@ const router = new Router();
 // router.prefix('/users')
 
 // 公共聊天区
-router.get('/', function (ctx: any, next: any) {
+router.get('/', function (ctx: Koa.Context, next: any) {
     let pathname = path.resolve(__dirname, '../../views/index.html'); 
     ctx.response.type = 'html';
     ctx.response.body = fs.createReadStream(pathname);
 })
 
 // 私密聊天
-router.get('/chat/:user/to/:anotheruser/:type', function (ctx: any, next: any) { // type为1 表示主动发起 为2表示被发起
+router.get('/chat/:user/to/:anotheruser/:type', function (ctx: Koa.Context, next: any) { // type为1 表示主动发起 为2表示被发起
     let pathname = path.resolve(__dirname, '../../views/privateChat.html'); 
     ctx.response.type = 'html';
     ctx.response.body = fs.createReadStream(pathname);
